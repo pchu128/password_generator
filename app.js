c9f36e3f6be69dea05ca294509324d7fc53e9666
Peter <peter03311@gmail.com>
2021-06-20 02:02:46 +0800

    add Procfile

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const port = 3000
+const port = process.env.PORT || 3000
 const generatePassword = require('./generatePassword')
 
 // setting template engine
